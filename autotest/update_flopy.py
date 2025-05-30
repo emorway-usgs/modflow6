5e34dc987d1abfcb063d217eec7f0809ce332147
wpbonelli <wbonelli@ucar.edu>
2025-05-30 15:41:51 -0400

    chore(update_flopy): remove unneeded steps from update_flopy.py (#2330)
    No longer need to delete/copy the flopy DFN directory since we removed DFNs from flopy.
    
    Also, just invoke flopy codegen directly in the update-flopy pixi task like python -m flopy.mf6.utils.generate_classes --dfnpath doc/mf6io/mf6ivar/dfn, instead of running update_flopy.py. The script doesn't do much anymore, besides saving some typing. I considered removing it, but I know not everyone uses pixi.

--- autotest/update_flopy.py
+++ autotest/update_flopy.py
@@ -1,13 +1,11 @@
 import argparse
 import importlib
 import os
-import shutil
 import subprocess
 import sys
 from pathlib import Path
 
 import flopy
-import pytest
 from conftest import project_root_path
 
 dfn_path = project_root_path / "doc" / "mf6io" / "mf6ivar" / "dfn"
@@ -23,32 +21,6 @@ def test_delete_mf6():
     delete_files(files, pth, exclude="mfsimulation.py")
 
 
-@pytest.mark.order(after="test_delete_mf6")
-def test_delete_dfn():
-    pth = os.path.join(fpy_path, "mf6", "data", "dfn")
-    files = [
-        entry for entry in os.listdir(pth) if os.path.isfile(os.path.join(pth, entry))
-    ]
-    delete_files(files, pth, exclude="flopy.dfn")
-
-
-@pytest.mark.order(after="test_delete_dfn")
-@pytest.mark.parametrize("path", [dfn_path])
-def test_copy_dfn(path):
-    files = [
-        entry for entry in os.listdir(path) if os.path.isfile(os.path.join(path, entry))
-    ]
-    pth1 = os.path.join(fpy_path, "mf6", "data", "dfn")
-    for fn in files:
-        ext = os.path.splitext(fn)[1].lower()
-        if "dfn" in ext:
-            fpth0 = os.path.join(path, fn)
-            fpth1 = os.path.join(pth1, fn)
-            print(f'copying {fn} from "{path}" to "{pth1}"')
-            shutil.copyfile(fpth0, fpth1)
-
-
-@pytest.mark.order(after="test_copy_dfn")
 def test_create_packages():
     # get list of files in mf6/modflow
     pth = os.path.join(fpy_path, "mf6", "modflow")
@@ -89,7 +61,7 @@ def list_files(pth, exts=["py"]):
             print(f"    {idx:5d} - {fn}")
 
 
-def delete_files(files, pth, allow_failure=False, exclude=None):
+def delete_files(files, pth, exclude=None):
     if exclude is None:
         exclude = []
     else:
@@ -105,9 +77,6 @@ def delete_files(files, pth, allow_failure=False, exclude=None):
             os.remove(fpth)
         except:
             print(f"could not remove...{fn}")
-            if not allow_failure:
-                return False
-    return True
 
 
 if __name__ == "__main__":
@@ -119,6 +88,4 @@ if __name__ == "__main__":
     print(f"Updating flopy packages from DFN files in: {path}")
 
     test_delete_mf6()
-    test_delete_dfn()
-    test_copy_dfn(path)
     test_create_packages()
