1bcbe2457d60945a53e76cf7fc048f54d468af31
wpbonelli <wbonelli@ucar.edu>
2025-09-29 16:34:55 -0400

    chore(release): update version number, reset release notes

--- doc/version.py
+++ doc/version.py
@@ -1,3 +1,3 @@
 # MODFLOW 6 version file automatically created using...update_version.py
-# created on...September 29, 2025 10:40:09
-__version__ = "6.6.3"
+# created on...September 29, 2025 16:33:48
+__version__ = "6.7.0.dev3"
